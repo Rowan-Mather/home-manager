c5938ee71f5cd2df6b6ddac0212acb6c05445205
Rowan Mather <rowan.m.mather@gmail.com>
2025-03-25 14:46:55 +0000

    Add flake

--- home.nix
+++ home.nix
@@ -6,8 +6,6 @@
   home.username = "rowan";
   home.homeDirectory = "/home/rowan";
   
-  #virtualisation.virtualbox.host.enable = true;
-  
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
