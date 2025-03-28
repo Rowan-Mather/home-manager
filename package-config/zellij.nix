ccb1a31bc49da36542eb859f2491e78267fd9673
Rowan Mather <rowan.m.mather@gmail.com>
2025-03-28 17:11:58 +0000

    Default zellij config

--- package-config/zellij.nix
+++ package-config/zellij.nix
@@ -3,7 +3,6 @@
 {
   programs.zellij.enable = true;
   programs.zellij.enableBashIntegration = true;
-  programs.zellij.attachExistingSession = true;
-  programs.zellij.exitShellOnExit = true;
+  # example zellij config: https://github.com/DieracDelta/flakes/blob/6820e64acf5e4db863dcf8840e798b746ee7a150/home/home.nix#L38
   #programs.zellij.settings = builtins.readFile ./zellij-extra;
 }
