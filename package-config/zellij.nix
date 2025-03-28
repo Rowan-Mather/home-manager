b140a02122e9d415e24cfae75705f9c61f5e2b7e
Rowan Mather <rowan.m.mather@gmail.com>
2025-03-28 17:16:41 +0000

    Remove uneeded (for now) zellij-extra

--- package-config/zellij.nix
+++ package-config/zellij.nix
@@ -4,5 +4,4 @@
   programs.zellij.enable = true;
   programs.zellij.enableBashIntegration = true;
   # example zellij config: https://github.com/DieracDelta/flakes/blob/6820e64acf5e4db863dcf8840e798b746ee7a150/home/home.nix#L38
-  #programs.zellij.settings = builtins.readFile ./zellij-extra;
 }
